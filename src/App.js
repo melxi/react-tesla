b6b2e92b19d5c876bc820a3f18797d4b49c00e44
Melxi <magomed.futijev@gmail.com>
2020-03-14 17:37:33 +0200

    initialize project

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+
+  return <div className="container">hello webpack</div>
+}
+
+export default App
